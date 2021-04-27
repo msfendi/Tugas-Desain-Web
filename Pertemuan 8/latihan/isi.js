97d4283d372cc2b18707a9f83b4ee2b0b3a7434a
MohamadSyarifudinEfendi-105 <efendisyarifudin1@gmail.com>
2021-04-27 15:06:18 +0700

    Pertemuan 8

---
+++ Pertemuan 8/latihan/isi.js
@@ -0,0 +1,7 @@
+document.write("<center>");
+document.write("<hr width=600 color=blue>");
+document.write("<h1>Belajar javascript</h1>");
+document.write("<h2>PTIK UNS 2020</h2>");
+document.write("<h4>NIM K3520043</h4>");
+document.write("<hr width=600 color brown>");
+document.write("</center>");
